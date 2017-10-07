53808019e27e14510c17d3874861c4900c694ad6
Omar Gonzalez <gonzalezrocha.omar@gmail.com>
2017-10-07 16:41:41 -0500

    device detection + ul styles

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,23 +1,35 @@
 (function (){
+    window.isMobile = false;
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        window.isMobile = true;
+    }
+})(window);
+
+(function (){
     'use strict';
-    var wWidth = $(window).width();
+    var eventKind = 'click';
+    if(isMobile){
+        eventKind = 'touchstart';
+    }
     var toggle = true;
     var mobileBreakPoint = 768;
-    $('#toggle-menu').on('touchstart', function(){
-        if(wWidth < mobileBreakPoint && toggle){
+    $('#toggle-menu').on(eventKind, function(){
+        if($(window).width() < mobileBreakPoint && toggle){
             toggle = false;
             $("#sidebar").animate({
                 width: '85%'
             }, 350, function() {
                 //animation complete
                 $('#close-menu').css('display','block');
+                $('.sidebar-content').css('display','block');
             });
         }
     });
-    $('#close-menu').on('touchstart', function(){
+    $('#close-menu').on(eventKind, function(){
         if(!toggle){
             toggle = true;
             $('#close-menu').css('display','none');
+            $('.sidebar-content').css('display','none');
             $('#sidebar').animate({
                 width:'44px'
             }, 350);
