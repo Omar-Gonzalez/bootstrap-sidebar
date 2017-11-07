61074b62e47c0e402dfd4f0ac473f89a21c62686
Omar Gonzalez <gonzalezrocha.omar@gmail.com>
2017-11-06 19:00:02 -0600

    fixed bug when loading with innerWidth of 768 px

--- dist/sidebar.build.js
+++ dist/sidebar.build.js
@@ -1,82 +1,101 @@
 "use strict";
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 /**
-* Omar Gonzalez - 17-10-2017 - Copyright MIT
-* ES6 Sidebar-Bootsrap Source 
-*/
+ * Omar Gonzalez - 17-10-2017 - Copyright MIT
+ * ES6 Sidebar-Bootsrap Source 
+ */
 
-var SideBar =
-/**
-* SideBar Component - props:
-* - this.updating : weather the sidebar is updating state
-* - this.state : weather the sidebar is open or close
-* - this.mobileBreakPoint : grid break point for mobile devices
-* SideBar - methods:
-* - update() - close or open the side bar
-* - screenSizeEvent() - attach event listener screen width change
-* - setDesktopMode() - set destkop mode if necesary
-*/
-function SideBar() {
-    var _this = this;
+var SideBar = function () {
+    /**
+     * SideBar Component - props:
+     * - this.updating : weather the sidebar is updating state
+     * - this.state : weather the sidebar is open or close
+     * - this.mobileBreakPoint : grid break point for mobile devices
+     * SideBar - methods:
+     * - update() - close or open the side bar
+     * - screenSizeEvent() - attach event listener screen width change
+     * - setDesktopMode() - set destkop mode if necesary
+     */
+    function SideBar() {
+        _classCallCheck(this, SideBar);
 
-    _classCallCheck(this, SideBar);
+        //Props
+        this.updating = false;
+        this.state = "close";
+        this.mobileBreakPoint = 768;
+        //Init methods
+        this.screenSizeEvent();
+    }
 
-    this.update = function () {
-        if (!_this.updating) {
-            if (_this.state === "close") {
-                //Open the SB
-                _this.updating = true;
-                $("#sidebar").animate({
-                    width: '85%'
-                }, 350, function () {
-                    //animation complete
-                    $('.sb-close').css('display', 'block');
-                    $('.sidebar-content').css('display', 'block');
-                    _this.updating = false;
-                    _this.state = "open";
-                });
+    _createClass(SideBar, [{
+        key: "update",
+        value: function update() {
+            var _this = this;
+
+            if (!this.updating) {
+                if (this.state === "close") {
+                    //Open the SB
+                    this.updating = true;
+                    $("#sidebar").animate({
+                        width: '85%'
+                    }, 350, function () {
+                        //animation complete
+                        $('.sb-close').css('display', 'block');
+                        $('.sidebar-content').css('display', 'block');
+                        _this.updating = false;
+                        _this.state = "open";
+                    });
+                }
+                if (this.state === "open") {
+                    //Close the SB
+                    this.updating = true;
+                    $('.sb-close').css('display', 'none');
+                    $('.sidebar-content').css('display', 'none');
+                    $('#sidebar').animate({
+                        width: '44px'
+                    }, 350, function () {
+                        //animation complete
+                        _this.updating = false;
+                        _this.state = "close";
+                    });
+                }
+            } else {
+                console.log("sb-updating");
             }
-            if (_this.state === "open") {
-                //Close the SB
-                _this.updating = true;
-                $('.sb-close').css('display', 'none');
-                $('.sidebar-content').css('display', 'none');
-                $('#sidebar').animate({
-                    width: '44px'
-                }, 350, function () {
-                    //animation complete
-                    _this.updating = false;
-                    _this.state = "close";
-                });
-            }
-        } else {
-            console.log("sb-updating");
         }
-    };
+    }, {
+        key: "screenSizeEvent",
+        value: function screenSizeEvent() {
+            var _this2 = this;
 
-    this.screenSizeEvent = function () {
-        $(window).on('resize', function () {
-            if ($(window).width() > _this.mobileBreakPoint) {
+            this.resize();
+            $(window).on('resize', function () {
+                _this2.resize();
+            });
+        }
+    }, {
+        key: "resize",
+        value: function resize() {
+            if ($(window).width() >= this.mobileBreakPoint) {
+                $('.sidebar-nav').hide();
                 $('#sidebar').css('width', '240px');
                 $('.sb-close').css('display', 'none');
                 $('.sidebar-content').css('display', 'block');
             } else {
+                $('.sidebar-nav').show();
                 $('#sidebar').css('width', '44px');
                 $('.sb-close').css('display', 'none');
                 $('.sidebar-content').css('display', 'none');
             }
-        });
-    };
+        }
+    }]);
 
-    //Props
-    this.updating = false;
-    this.state = "close";
-    this.mobileBreakPoint = 768;
-    //Init methods
-    this.screenSizeEvent();
-};
+    return SideBar;
+}();
 
 var sb = new SideBar();
 //# sourceMappingURL=sidebar.build.js.map
--- src/sidebar.js
+++ src/sidebar.js
@@ -1,20 +1,20 @@
 /**
-* Omar Gonzalez - 17-10-2017 - Copyright MIT
-* ES6 Sidebar-Bootsrap Source 
-*/
+ * Omar Gonzalez - 17-10-2017 - Copyright MIT
+ * ES6 Sidebar-Bootsrap Source 
+ */
 
 class SideBar {
     /**
-    * SideBar Component - props:
-    * - this.updating : weather the sidebar is updating state
-    * - this.state : weather the sidebar is open or close
-    * - this.mobileBreakPoint : grid break point for mobile devices
-    * SideBar - methods:
-    * - update() - close or open the side bar
-    * - screenSizeEvent() - attach event listener screen width change
-    * - setDesktopMode() - set destkop mode if necesary
-    */
-    constructor(){
+     * SideBar Component - props:
+     * - this.updating : weather the sidebar is updating state
+     * - this.state : weather the sidebar is open or close
+     * - this.mobileBreakPoint : grid break point for mobile devices
+     * SideBar - methods:
+     * - update() - close or open the side bar
+     * - screenSizeEvent() - attach event listener screen width change
+     * - setDesktopMode() - set destkop mode if necesary
+     */
+    constructor() {
         //Props
         this.updating = false;
         this.state = "close";
@@ -23,49 +23,56 @@ class SideBar {
         this.screenSizeEvent();
     }
 
-    update = () => {
-        if (!this.updating){
-            if (this.state === "close"){ //Open the SB
+    update() {
+        if (!this.updating) {
+            if (this.state === "close") { //Open the SB
                 this.updating = true;
                 $("#sidebar").animate({
                     width: '85%'
                 }, 350, () => {
                     //animation complete
-                    $('.sb-close').css('display','block');
-                    $('.sidebar-content').css('display','block');
+                    $('.sb-close').css('display', 'block');
+                    $('.sidebar-content').css('display', 'block');
                     this.updating = false;
                     this.state = "open";
                 });
             }
-            if (this.state === "open"){ //Close the SB
+            if (this.state === "open") { //Close the SB
                 this.updating = true;
-                $('.sb-close').css('display','none');
-                $('.sidebar-content').css('display','none');
+                $('.sb-close').css('display', 'none');
+                $('.sidebar-content').css('display', 'none');
                 $('#sidebar').animate({
-                    width:'44px'
+                    width: '44px'
                 }, 350, () => {
                     //animation complete
-                    this.updating  = false;
+                    this.updating = false;
                     this.state = "close";
-                });            
-            }        
-        }else{
+                });
+            }
+        } else {
             console.log("sb-updating");
         }
     }
 
-    screenSizeEvent = () => {
-        $(window).on('resize',()=>{
-            if($(window).width() > this.mobileBreakPoint){
-                $('#sidebar').css('width','240px');
-                $('.sb-close').css('display','none');
-                $('.sidebar-content').css('display','block');            
-            }else{
-                $('#sidebar').css('width','44px');
-                $('.sb-close').css('display','none');
-                $('.sidebar-content').css('display','none');            
-            }
+    screenSizeEvent() {
+        this.resize();
+        $(window).on('resize', () => {
+            this.resize();
         });
+    }
+
+    resize() {
+        if ($(window).width() >= this.mobileBreakPoint) {
+            $('.sidebar-nav').hide();
+            $('#sidebar').css('width', '240px');
+            $('.sb-close').css('display', 'none');
+            $('.sidebar-content').css('display', 'block');
+        } else {
+            $('.sidebar-nav').show();
+            $('#sidebar').css('width', '44px');
+            $('.sb-close').css('display', 'none');
+            $('.sidebar-content').css('display', 'none');
+        }
     }
 }
 
